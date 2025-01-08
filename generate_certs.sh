9cff2438eb03fd9b880e1e073321d5b8c400bd79
Dario Riccardi <riccardi94dario@gmail.com>
2025-01-08 15:30:12 +0100

    up

--- generate_certs.sh
+++ generate_certs.sh
@@ -4,23 +4,23 @@
 set -e
 
 # Genera il certificato della CA
-openssl req -x509 -new -newkey dilithium5 -keyout /certs/CA.key -out /certs/CA.crt -nodes -days 365 -config /openssl.cnf -subj "/CN=oqstest CA" -extensions v3_ca 
+openssl req -x509 -new -newkey dilithium5 -keyout /nginx/certs/CA.key -out /nginx/certs/CA.crt -nodes -days 365 -config /openssl.cnf -subj "/CN=oqstest CA" -extensions v3_ca 
 
 # Genera la richiesta di firma per il certificato del server
-openssl req -new -newkey dilithium5 -keyout /certs/server.key -out /certs/server.csr -nodes -config /openssl.cnf -subj "/CN=nginx_pq" -extensions v3_req 
+openssl req -new -newkey dilithium5 -keyout /nginx/certs/server.key -out /nginx/certs/server.csr -nodes -config /openssl.cnf -subj "/CN=nginx_pq" -extensions v3_req 
 
 # Firma il certificato del server usando la CA
-openssl x509 -req -in /certs/server.csr -out /certs/server.crt -CA /certs/CA.crt -CAkey /certs/CA.key -CAcreateserial -days 365
+openssl x509 -req -in /nginx/certs/server.csr -out /nginx/certs/server.crt -CA /nginx/certs/CA.crt -CAkey /nginx/certs/CA.key -CAcreateserial -days 365
 
 # Crea la catena di certificati
-cat /certs/server.crt > /certs/qsc-ca-chain.crt
-cat /certs/CA.crt >> /certs/qsc-ca-chain.crt
+cat /nginx/certs/server.crt > /nginx/certs/qsc-ca-chain.crt
+cat /nginx/certs/CA.crt >> /nginx/certs/qsc-ca-chain.crt
 
 # Imposta i permessi sui certificati generati
-chmod 644 /certs/server.key
-chmod 644 /certs/qsc-ca-chain.crt
-chmod 644 /certs/CA.crt
-chmod 644 /certs/server.crt
+chmod 644 /nginx/certs/server.key
+chmod 644 /nginx/certs/qsc-ca-chain.crt
+chmod 644 /nginx/certs/CA.crt
+chmod 644 /nginx/certs/server.crt
 
 echo "Certificati generati, catena creata e permessi impostati correttamente!"
 
@@ -29,11 +29,11 @@ if [ "$VERIFY_CERTS" = "1" ]; then
   echo "Esecuzione dei controlli sui certificati..."
 
   # Verifica il certificato del server
-  openssl verify -CAfile /certs/CA.crt /certs/server.crt
+  openssl verify -CAfile /nginx/certs/CA.crt /nginx/certs/server.crt
 
   # Visualizza informazioni sulla chiave
-  openssl x509 -in /certs/server.crt -text -noout
-  openssl x509 -in /certs/server.crt -noout -text | grep "Public Key Algorithm"
+  openssl x509 -in /nginx/certs/server.crt -text -noout
+  openssl x509 -in /nginx/certs/server.crt -noout -text | grep "Public Key Algorithm"
 
   echo "Controlli completati!"
 fi
