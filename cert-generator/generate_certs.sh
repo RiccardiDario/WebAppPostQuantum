f70311a56d90805a84ef189ab2ea8a7eabc55734
RiccardiDario <riccardi94dario@gmail.com>
2025-05-04 19:21:28 +0200

    up

--- cert-generator/generate_certs.sh
+++ cert-generator/generate_certs.sh
@@ -5,10 +5,11 @@ set -e
 
 CA_KEY="/certs/CA.key"; CA_CERT="/certs/CA.crt"; SERVER_KEY="/certs/server.key"
 SERVER_CERT="/certs/server.crt"; SERVER_CHAIN="/certs/qsc-ca-chain.crt"; SERVER_CSR="/certs/server.csr"
+EXTFILE="/tmp/ext.cnf"
 
 openssl_pkey() {
   case "$1" in
-    rsa-pss-*) openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out "$2" ;;
+    rsa-pss-*) openssl genrsa -out "$2" 4096 ;;
     ecdsa_p256) openssl ecparam -genkey -name prime256v1 -out "$2" ;;
     ecdsa_p384) openssl ecparam -genkey -name secp384r1 -out "$2" ;;
     ecdsa_p521) openssl ecparam -genkey -name secp521r1 -out "$2" ;;
@@ -32,14 +33,12 @@ sigopts() {
 }
 
 echo "Generazione certificati..."
-
 openssl_pkey "$SIGNATURE_ALGO" "$CA_KEY"
 SIGOPT_CA=$(sigopts "$SIGNATURE_ALGO")
 openssl req -x509 -new -key "$CA_KEY" -out "$CA_CERT" -nodes -days 365 \
   -subj "/CN=oqstest CA" \
   -addext "basicConstraints=critical,CA:TRUE" \
-  -addext "keyUsage=critical,keyCertSign,cRLSign" \
-  $SIGOPT_CA
+  -addext "keyUsage=critical,keyCertSign,cRLSign" $SIGOPT_CA
 
 openssl_pkey "$SIGNATURE_ALGO" "$SERVER_KEY"
 openssl req -new -key "$SERVER_KEY" -out "$SERVER_CSR" \
@@ -49,24 +48,20 @@ openssl req -new -key "$SERVER_KEY" -out "$SERVER_CSR" \
   -addext "extendedKeyUsage=serverAuth"
 
 SIGOPT=$(sigopts "$SIGNATURE_ALGO")
-openssl x509 -req -in "$SERVER_CSR" -out "$SERVER_CERT" -CA "$CA_CERT" -CAkey "$CA_KEY" -CAcreateserial -days 365 \
-  -extfile <(cat <<EOF
+cat <<EOF > "$EXTFILE"
 basicConstraints=critical,CA:FALSE
 keyUsage=critical,digitalSignature,keyEncipherment
 extendedKeyUsage=serverAuth
 EOF
-) \
-  $SIGOPT
 
+openssl x509 -req -in "$SERVER_CSR" -out "$SERVER_CERT" -CA "$CA_CERT" -CAkey "$CA_KEY" \
+  -CAcreateserial -days 365 -extfile "$EXTFILE" $SIGOPT
 
 cat "$SERVER_CERT" "$CA_CERT" > "$SERVER_CHAIN"
 chmod 644 "$SERVER_KEY" "$SERVER_CHAIN" "$CA_CERT" "$SERVER_CERT"
 
 echo "Certificati generati con successo!"
-
-[ "$VERIFY_CERTS" = "1" ] && {
-  echo "Verifica certificati..."
-  openssl verify -CAfile "$CA_CERT" "$SERVER_CERT"
-  openssl x509 -in "$SERVER_CERT" -text -noout | grep -A1 "Signature Algorithm"
-  echo "Verifica completata!"
-}
+echo "Verifica certificati..."
+openssl verify -CAfile "$CA_CERT" "$SERVER_CERT"
+openssl x509 -in "$SERVER_CERT" -text -noout | grep -A1 "Signature Algorithm"
+echo "Verifica completata!"
